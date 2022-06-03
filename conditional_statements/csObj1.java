046d06c270f6800ce94be2942b6cf08ca6367f50
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-02 17:53:19 -0800

    Print Values to Compare 1.0

---
+++ conditional_statements/csObj1.java
@@ -0,0 +1,17 @@
+/* Hello World (Java) */
+
+class csObj1 {
+  public static void main(String[] args) {
+    // First Variable
+    int v1 = 13;
+    // Second Variable
+    int v2 = 343;
+
+    // Compare Variables
+
+    // Print which variable is bigger
+
+      System.out.println("V1: " + v1 + "   V2: " + v2);
+      // System.out.println("___ is bigger");
+  }
+}
