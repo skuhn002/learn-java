c9b95f9a0bbcecf54752d1df0e3faf915756da79
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-06 18:49:51 -0800

    Do While Loop 1.0

--- loops/loop1.java
+++ loops/loop1.java
@@ -4,11 +4,11 @@ public class loop1 {
    public static void main(String args[]) {
       int x = 10;
 
-      while( x < 20 ) {
+      do {
          System.out.print("value of x : " + x );
          x++;
          System.out.print("\n");
-      }
+      } while( x < 20 );
 
       for(int y = 10; y < 20; y = y + 1) {
          System.out.print("value of y : " + y );
