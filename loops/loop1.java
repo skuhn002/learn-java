2a1077c300c76a5c145919488bbe16baa4b15ede
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-06 18:43:46 -0800

    For Loop 1.0

--- loops/loop1.java
+++ loops/loop1.java
@@ -1,13 +1,17 @@
 /* Loops */
 
 public class loop1 {
-
    public static void main(String args[]) {
       int x = 10;
 
       while( x < 20 ) {
          System.out.print("value of x : " + x );
          x++;
+         System.out.print("\n");
+      }
+
+      for(int y = 10; y < 20; y = y + 1) {
+         System.out.print("value of y : " + y );
          System.out.print("\n");
       }
    }
