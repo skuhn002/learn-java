c9639ef4bb286ea87379d9c3692e08842739c287
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-06 18:33:18 -0800

    While Example 1.0

---
+++ loops/loop1.java
@@ -0,0 +1,14 @@
+/* Loops */
+
+public class loop1 {
+
+   public static void main(String args[]) {
+      int x = 10;
+
+      while( x < 20 ) {
+         System.out.print("value of x : " + x );
+         x++;
+         System.out.print("\n");
+      }
+   }
+}
