1ee60071b91c74fa5ab240ee75cf235dd17e7ecf
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-22 23:58:08 -0800

    KW 'super' example 1.0

--- keywords/kwd.java
+++ keywords/kwd.java
@@ -2,20 +2,35 @@
 
 class Vehicle {
   int n_whl;
+  Boolean can_tow = false;
 
   Vehicle(int n_whl){
     this.n_whl = n_whl;
   }
 
-  public void check_wheels(){
+  public void check(){
     System.out.println("This vehicle has " + n_whl + " wheels");
+  }
+}
+
+class Truck extends Vehicle {
+  Boolean can_tow = true;
+
+  Truck(){
+    super(4);
+  }
+
+  void checkParent(){
+    System.out.println("I am a truck. Can I tow? " + Boolean.toString(this.can_tow));
+    System.out.println("Can parent class (Vehicle) tow? " + Boolean.toString(super.can_tow));
   }
 }
 
 public class kwd {
   public static void main(String args[]){
-    Vehicle ride1 = new Vehicle(5);
-    ride1.check_wheels();
-    // System.out.println("Vehicle Wheel No: " + this.n_whl);
+    Vehicle ride1 = new Vehicle(4);
+    Truck ride2 = new Truck();
+    ride1.check();
+    ride2.checkParent();
   }
 }
