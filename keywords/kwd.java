b7a0b68ed7a09ad5cf1c6fcb6c7219fe3d09875c
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-28 13:00:15 -0800

    KW 'final' example 1.0

--- keywords/kwd.java
+++ keywords/kwd.java
@@ -4,14 +4,18 @@ class Vehicle {
   int n_whl;
   Boolean can_tow = false;
   static String fuelType = "unleaded";
+  final Boolean parkable = true;
 
   Vehicle(int n_whl){
     this.n_whl = n_whl;
   }
 
   public void check(){
-    System.out.println("This vehicle has " + n_whl + " wheels");
-    System.out.println("This vehicle takes " + fuelType + " fuel");
+    System.out.println("");
+    System.out.println("This vehicle has " + this.n_whl + " wheels");
+    System.out.println("This vehicle takes " + this.fuelType + " fuel");
+    System.out.println("Parkable: " + this.parkable);
+    System.out.println("");
   }
 }
 
@@ -30,13 +34,22 @@ class Truck extends Vehicle {
 
 public class kwd {
   public static void main(String args[]){
+    // Create Vehicle and Truck instance with 'new'
     Vehicle ride1 = new Vehicle(4);
     Truck ride2 = new Truck();
     ride1.check();
     ride2.check();
+
+    // Check Parent Value using 'super'
     ride2.checkParent();
+
+    // Change Static Variable fuelType
     Vehicle.fuelType = "diesel";
+
     ride1.check();
     ride2.check();
+
+    // Parkable is a 'final' variable
+    // ride1.parkable = false; // Test: can't overide final variable parkable
   }
 }
