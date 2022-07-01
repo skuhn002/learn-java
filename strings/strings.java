4362a3f5be26b27bcf30958690cc88b041905a46
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-30 19:05:07 -0800

    Boolean Spell Checker 1.0

--- strings/strings.java
+++ strings/strings.java
@@ -1,10 +1,21 @@
 /* String Manipulation */
 
-public class strings{
-  String resopnse = "cavyar"; //word to spell is caviar
+class SpellChecker{
+  String response = "cavyar"; //word to spell is caviar
   String answer = "caviar";
-  
-  public static void main(String args[]) {
-    System.out.println("Strings!!!");
+  Boolean isCorrect = false; //default to false
+
+  SpellChecker() {
+    // Tell the user if the response was correctly spelled (Yes or No)
+    System.out.println("Spell Checker Initialized");
+    this.isCorrect = response.equals(answer);
+    System.out.println(this.isCorrect.toString());
+  }
+}
+
+public class strings{
+  public static void main(String[] args) {
+    System.out.println("Main Initialized");
+    SpellChecker check = new SpellChecker();
   }
 }
