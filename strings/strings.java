d03b201f1a6fbb2c48dc1e22972565382546949c
skuhn002 <kuhnsamuel2@gmail.com>
2022-06-30 18:24:48 -0800

    Boilerplate 1.0

---
+++ strings/strings.java
@@ -0,0 +1,10 @@
+/* String Manipulation */
+
+public class strings{
+  String resopnse = "cavyar"; //word to spell is caviar
+  String answer = "caviar";
+  
+  public static void main(String args[]) {
+    System.out.println("Strings!!!");
+  }
+}
