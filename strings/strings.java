aa523a7ec612a78a562745db854bc89b30023916
skuhn002 <kuhnsamuel2@gmail.com>
2022-07-05 03:04:44 -0800

    User Feedback 2.0

--- strings/strings.java
+++ strings/strings.java
@@ -1,19 +1,38 @@
 /* String Manipulation */
 
 class SpellChecker{
-  String response = "cavyar"; //word to spell is caviar
+  String response = "ccavyar"; //word to spell is caviar
   String answer = "caviar";
   Boolean isCorrect = false; //default to false
 
   SpellChecker() {
     // Tell the user if the response was correctly spelled (Yes or No)
     System.out.println("Spell Checker Initialized\n");
-    this.isCorrect = response.equals(answer);
+    this.isCorrect = false; // response.equals(answer);
 
     if(this.isCorrect) {
       System.out.println("Answer: " + this.answer);
       System.out.println("Response: " + this.response);
     } else {
+      // Specify Mispelling
+      // Too Many Letters
+      if(response.length() > answer.length()) {
+        System.out.println("Too Many Letters");
+      }
+      // Too Few Letters
+      if(answer.length() > response.length()) {
+        System.out.println("Too Few Letters");
+      }
+      // Incorrect Letters - tell user all letters are in/correct if they had too few letters
+      //  All letters entered are correct - - - no response
+      //      for loop through length of answer
+      //        check if letters at counter/index match
+      //          if match - loop
+      //          else don't match - store value of index of incorrect letter (firstBad)
+      //      send feedback to user
+      //        firstBad has a value - tell user what the first incorrect letter was
+      //        firstBad is null - [no feedback]
+
       System.out.println("Answer: " + this.answer);
       System.out.println("Response: " + this.response);
     }
