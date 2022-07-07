d3c9e1f52e684faf367bbef5302d3329e9e7b25e
skuhn002 <kuhnsamuel2@gmail.com>
2022-07-06 18:35:59 -0800

    User Feedback 3.0

--- strings/strings.java
+++ strings/strings.java
@@ -1,7 +1,8 @@
 /* String Manipulation */
+// This program compares a response to an answer. The response is meant to be a response to a spelling question.
 
 class SpellChecker{
-  String response = "cavyar"; //word to spell is caviar
+  String response = "cavya"; //word to spell is caviar
   String answer = "caviar";
   Boolean isCorrect = false; //default to false
   Boolean[] checkArr = new Boolean[answer.length()]; //check array - 0's are incorrect letters, and 1's are correct letters
@@ -24,37 +25,31 @@ class SpellChecker{
       if(answer.length() > response.length()) {
         System.out.println("Too Few Letters");
       }
-      // Incorrect Letters - tell user all letters are in/correct if they had too few letters???
-      //  All letters entered are correct - - - no response
-      //      for loop through length of answer
-      //        check if letters at counter/index match
-      //          if match - loop
-      //          else don't match - store value of index of incorrect letter (firstBad)
-      //      send feedback to user
-      //        firstBad has a value - tell user what the first incorrect letter was
-      //        firstBad is null - [no feedback]
 
       for(int cnt = 0; cnt < answer.length(); cnt++){
-        System.out.println("Count is: " + (cnt + 1));
+        // System.out.println("Count is: " + (cnt + 1));
+
         // check for inccorect letter
-        if(answer.charAt(cnt) == response.charAt(cnt)){
-          System.out.println("Letter " + cnt + " (" + response.charAt(cnt) + ") is correct"); //test 1
-          checkArr[cnt] = true;
+        if(cnt < response.length()){
+          if(answer.charAt(cnt) == response.charAt(cnt)){
+            System.out.println("Letter " + cnt + " (" + response.charAt(cnt) + ") is correct"); //test 1
+            checkArr[cnt] = true;
+          } else {
+            System.out.println("Letter " + cnt + " (" + response.charAt(cnt) + ") is incorrect");
+            checkArr[cnt] = false;
+          }
         } else {
-          System.out.println("Letter " + cnt + " (" + response.charAt(cnt) + ") is incorrect");
+          System.out.println("Letter " + cnt + " is missing");
           checkArr[cnt] = false;
         }
       }
-
-      // System.out.println("Answer: " + this.answer);
-      // System.out.println("Response: " + this.response);
 
       // Show user which letters they got correct with "_" inbetween
       System.out.println("\nThis is what you got correct");
 
       for(int cnt = 0; cnt < checkArr.length; cnt++){
         if(checkArr[cnt]){
-          System.out.print(answer.charAt(cnt));
+          System.out.print(response.charAt(cnt));
         } else {
           System.out.print(" _ ");
         }
