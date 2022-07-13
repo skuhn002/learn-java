8e1f1a1836f06c8d48214ea83c7e2b6b0a1d3fd1
skuhn002 <kuhnsamuel2@gmail.com>
2022-07-13 10:48:26 -0800

    Tester2 PKG 1.0

--- pkg/tester.java
+++ pkg/tester.java
@@ -1,4 +1,5 @@
 package pkg;
+import pkg2.tester2;
 
 public class tester {
   public static void main(String args[]){
---
+++ pkg2/tester2.java
@@ -0,0 +1,7 @@
+package pkg2;
+
+public class tester2 {
+  public static void main(String args[]){
+    System.out.println("Hello from tester2");
+  }
+}
