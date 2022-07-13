d27ebff009e5ccb02a3a2f3bc83c819aea4e3cc3
skuhn002 <kuhnsamuel2@gmail.com>
2022-07-13 12:05:59 -0800

    Import/Use Second PKG 1.0

--- pkg/tester.java
+++ pkg/tester.java
@@ -1,8 +1,9 @@
 package pkg;
-import pkg2.tester2;
+import pkg2.*;
 
 public class tester {
   public static void main(String args[]){
     System.out.println("Hello");
+    pkg2.tester2.msg();
   }
 }
--- pkg2/tester2.java
+++ pkg2/tester2.java
@@ -2,6 +2,9 @@ package pkg2;
 
 public class tester2 {
   public static void main(String args[]){
-    System.out.println("Hello from tester2");
+  }
+
+  public static void msg(){
+    System.out.println("Hello from tester2 (parallel folder)");
   }
 }
